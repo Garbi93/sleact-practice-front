8e39090c34397d980d3d3771fdd63862cd35c270
Garbi93 <iseung2093@gmail.com>
2024-05-16 18:50:19 +0900

    5/16 로그인 페이지 만들기

--- front/pages/SignUp/index.tsx
+++ front/pages/SignUp/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import { Form, Success, Error, Header, Input, Label, LinkContainer, Button } from './style';
 import useInput from '@hooks/useInput';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   const [email, onChangeEmail] = useInput('');
@@ -98,7 +99,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
   );
