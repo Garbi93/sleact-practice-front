2360727c3c3e87c37d837bf2c698a65ca2051c75
Garbi93 <iseung2093@gmail.com>
2024-05-20 19:10:46 +0900

    5/20 라우터 주소 설계 (라우트 파라미터)

--- front/components/CreateChannelModal/index.tsx
+++ front/components/CreateChannelModal/index.tsx
@@ -2,16 +2,61 @@ import React, { useCallback, VFC } from 'react';
 import Modal from '@components/Modal';
 import { Button, Input, Label } from '@pages/SignUp/styles';
 import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
 
 interface Props {
   show: boolean;
   onCloseModal: () => void;
+  setShowCreatChannelModal: (flag: boolean) => void;
 }
 
-const CreateChannelModal: VFC<Props> = ({ show, onCloseModal }) => {
-  const [newChannel, onChangeNewChannel] = useInput('');
+const CreateChannelModal: VFC<Props> = ({ show, onCloseModal, setShowCreatChannelModal }) => {
+  const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
 
-  const onCreateChannel = useCallback(() => {}, []);
+  const {
+    data: userData,
+    error,
+    mutate,
+  } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher, {
+    dedupingInterval: 10000,
+  });
+
+  const { data: channelData, mutate: mutateChannel } = useSWR<IChannel[]>(
+    userData ? `http://localhost:3095/api/workspaces/${workspace}/channels` : null,
+    fetcher,
+  );
+
+  const onCreateChannel = useCallback(
+    (e) => {
+      e.preventDefault();
+      axios
+        .post(
+          `http://localhost:3095/api/workspaces/${workspace}/channels`,
+          {
+            name: newChannel,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          setShowCreatChannelModal(false);
+          mutateChannel();
+          setNewChannel('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newChannel],
+  );
 
   return (
     <Modal show={show} onCloseModal={onCloseModal}>
