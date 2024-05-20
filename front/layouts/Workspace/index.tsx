a104aac1915debb4cfbb05b78f17bcf7b9e9ad2c
Garbi93 <iseung2093@gmail.com>
2024-05-20 14:57:37 +0900

    5/20 워크스페이스 만들기(중첩 라우터)

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, useCallback } from 'react';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import { Redirect } from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import {
   Channels,
   Chats,
@@ -15,6 +15,10 @@ import {
   WorkspaceWrapper,
 } from '@layouts/Workspace/styles';
 import gravatar from 'gravatar';
+import loadable from '@loadable/component';
+
+const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const Workspace: FC = ({ children }) => {
   const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher, {
@@ -51,9 +55,13 @@ const Workspace: FC = ({ children }) => {
           <WorkspaceName>Sleact</WorkspaceName>
           <MenuScroll>MenuScroll</MenuScroll>
         </Channels>
-        <Chats>Chats</Chats>
+        <Chats>
+          <Switch>
+            <Route path="/workspace/channel" component={Channel} />
+            <Route path="/workspace/dm" component={DirectMessage} />
+          </Switch>
+        </Chats>
       </WorkspaceWrapper>
-      {children}
     </div>
   );
 };
---
+++ front/pages/DirectMessage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DirectMessage = () => {
+  return <div>dm 페이지 </div>;
+};
+
+export default DirectMessage;
