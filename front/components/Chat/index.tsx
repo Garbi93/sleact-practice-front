3a65764366772d7a450860fc0e0ce192343fa8d0
Garbi93 <iseung2093@gmail.com>
2024-05-25 17:43:10 +0900

    5/25 타입 점검 (타입 가드)

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,5 +1,5 @@
 import React, { VFC, memo, useMemo } from 'react';
-import { IDM } from '@typings/db';
+import { IChat, IDM } from '@typings/db';
 import { ChatWrapper } from '@components/Chat/styles';
 import gravatar from 'gravatar';
 import dayjs from 'dayjs';
@@ -7,12 +7,12 @@ import regexifyString from 'regexify-string';
 import { Link, useParams } from 'react-router-dom';
 
 interface Props {
-  data: IDM;
+  data: IDM | IChat;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
   const { workspace } = useParams<{ workspace: string; channel: string }>();
-  const user = data.Sender;
+  const user = 'Sender' in data ? data.Sender : data.User;
 
   const result = useMemo(
     () =>
